ff0695257b1faf15f44d92efe3a6f55c98817d46
Babatunde Yusuf Folorunsho <babatundeyusuffolorunsho@gmail.com>
2022-07-09 19:17:06 +0100

    completing the mobile view of landing page

--- articonnect.js
+++ articonnect.js
@@ -1,10 +1,16 @@
 navBtn =document.getElementById("navBtn")
+cancel=document.getElementsByClassName("cancel")[0]
 navPhone =document.getElementsByClassName("navPhone")[0];
 
 navBtn.addEventListener("click",function(){
     // console.log("hello");
 
      navPhone.classList.toggle("active")
+})
+
+cancel.addEventListener("click",function(){
+    // console.log("helo")
+    navPhone.classList.remove("active")
 })
 
 
