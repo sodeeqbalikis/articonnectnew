988570bca7eae57a92b1c315148062c50d912795
Babatunde Yusuf Folorunsho <babatundeyusuffolorunsho@gmail.com>
2022-07-09 11:52:37 +0100

    chaging the landing page

---
+++ articonnect.js
@@ -0,0 +1,10 @@
+navBtn =document.getElementById("navBtn")
+navPhone =document.getElementsByClassName("navPhone")[0];
+
+navBtn.addEventListener("click",function(){
+    // console.log("hello");
+
+     navPhone.classList.toggle("active")
+})
+
+
